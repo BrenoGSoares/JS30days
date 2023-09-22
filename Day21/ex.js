1ff886490fcb19f98de824f0dcd1ae57d01b39d7
Breno <breno.givas@icloud.com>
2023-09-22 15:29:46 -0300

    class 21 completed

---
+++ Day21/ex.js
@@ -0,0 +1,28 @@
+//Crie um arquivo index.html e coloque quatro elementos p como acima: Obtenha o primeiro parágrafo usando document.querySelector(tagname) e tag name
+// Done
+
+// Obtenha cada um dos parágrafos usando document.querySelector('#id') e por seu id
+// Done
+
+// Obtenha todos os p como nodeList usando document.querySelectorAll(tagname) e pelo nome da tag
+// Done
+
+// Percorra o nodeList e obtenha o conteúdo do texto de cada parágrafo
+// Done
+
+// Defina um conteúdo de texto para o parágrafo quarto parágrafo, quarto parágrafo
+// Done
+
+// Defina o atributo id e class para todos os parágrafos usando diferentes métodos de configuração de atributos
+// Done
+
+// EX 2
+
+// Altere o estilo de cada parágrafo usando JavaScript (por exemplo, cor, plano de fundo, borda, tamanho da fonte, família da fonte)
+// Done
+
+// Selecione todos os parágrafos e percorra cada elemento e dê ao primeiro e terceiro parágrafo uma cor verde, e o segundo e o quarto parágrafo uma cor vermelha
+// Done
+
+// Defina o conteúdo do texto, id e classe para cada parágrafo
+// Done

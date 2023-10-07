760cb0978ecd6a102f429e6874b75dc3e1bebdc0
Breno <breno.givas@icloud.com>
2023-10-07 18:11:57 -0300

    Doing: class 28

--- Day28/scripts/main.js
+++ Day28/scripts/main.js
@@ -14,38 +14,70 @@ const meses = [
 ]
 
 const playerTable = document.querySelector('.flex-leader')
+const playerData = document.querySelectorAll('.text-input')
+
+function add() {
+  const newValue = parseInt(playerData[3].value) + 5
+  // Faça algo com o novo valor, por exemplo, atualize um elemento na página
+  console.log(`Novo valor após adicionar 5: ${newValue}`)
+}
+
+function sub() {
+  const newValue = parseInt(playerData[3].value) - 5
+  console.log(`Novo valor após subtrair 5: ${newValue}`)
+}
+
+function rem() {
+  const newValue = parseInt(playerData[3].value) - 0
+  console.log(`Novo valor após subtrair 5: ${newValue}`)
+}
 
 const addPlayer = () => {
-  const playerData = document.querySelectorAll('.text-input')
   const block = document.createElement('div')
+  const lineBreak = document.createElement('br')
   block.className = 'player'
   for (let i = 0; i < playerData.length; i++) {
     const data = document.createElement('h2')
-    const time = document.createElement('h3')
     data.className = 'data'
 
     if (i === 0) {
       const now = new Date()
-      time.textContent = `${
+      const timeSpan = document.createElement('span')
+
+      timeSpan.textContent = `${
         meses[now.getMonth()]
-      }, ${now.getFullYear()}, ${now.getHours()}:${now.getMinutes()}`
-      data.textContent = `${playerData[i].value} ${time.textContent}`
+      } ${now.getDay()}, ${now.getFullYear()}, ${now.getHours()}:${now.getMinutes()}`
+
+      data.appendChild(
+        document.createTextNode(
+          playerData[i].value + ' ' + playerData[i + 1].value
+        )
+      )
+      timeSpan.id = 'time'
+      data.appendChild(document.createTextNode(' '))
+      data.appendChild(lineBreak)
+      data.appendChild(timeSpan) // Anexe o tempo como um <span>
+
       data.id = 'firstValue'
+      i += 1
     } else if (i === 3) {
       data.textContent = parseInt(playerData[i].value)
     } else {
       data.textContent = playerData[i].value
     }
+
     block.appendChild(data)
   }
 
   const buttonTexts = ['xx', '+5', '-5']
+  const buttonFuncs = [rem, add, sub]
   const blockBtn = document.createElement('div')
 
   for (let i = 0; i < buttonTexts.length; i++) {
     const btnFunc = document.createElement('button')
     btnFunc.textContent = buttonTexts[i]
     btnFunc.className = 'btnFunc'
+    btnFunc.onclick = buttonFuncs[i]
     blockBtn.appendChild(btnFunc)
     block.appendChild(blockBtn)
   }
